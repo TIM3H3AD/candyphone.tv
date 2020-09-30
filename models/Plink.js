042b610450bf1402ead14381de0bfedec4d3e105
oddbank <fdic.313@gmail.com>
2020-09-30 17:17:39 -0400

    working on views

--- models/Plink.js
+++ models/Plink.js
@@ -3,7 +3,7 @@ const Timestamp = require("./Timestamp");
 const User = require("./User");
 
 const plinkSchema = new mongoose.Schema({
-   name : { type: String, required: true },
+   title : { type: String, required: true },
    description : { type: String, required: true, maxlength: 200 },
    imageUrl: { type: String,  required: true, validate: {
     validator: function(v) {
@@ -11,7 +11,6 @@ const plinkSchema = new mongoose.Schema({
     },
     message: props => `${props.value} is not a valid Url!`
   }, },
-  difficultyLevel: { type: Number, required: true , min:1,max:6 },
   timestamps:[{ type: mongoose.Schema.Types.ObjectId, ref: 'Timestamp' }],
   createdBy:{ type: mongoose.Schema.Types.ObjectId, ref: 'User', required:true }
 
